2b43865a442b69378a8349b02410fe8d8cc5adad
Sanjay <sanjutech9959@gmail.com>
2026-04-03 19:03:32 +0530

    setup mongodb connection with environment config

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import app from './app.js'
+import connectDB from './config/db.js'
 
 const PORT = process.env.PORT
+
+// Connecting to the database
+
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
