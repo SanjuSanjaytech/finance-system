4883ab1e39c8d01186acc785cd9758833b1cc29c
Sanjay <sanjutech9959@gmail.com>
2026-04-03 20:05:42 +0530

    implement user authentication with jwt and bcrypt

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import authRoutes from "./routes/authRoutes.js"
 
 
 dotenv.config()
@@ -9,6 +10,8 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use("/api/auth", authRoutes)
 
 
 // route
---
+++ controllers/authController.js
@@ -0,0 +1,70 @@
+import User from '../models/userModel.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '7d',
+    })
+}
+
+
+export const registerUser = async (req, res) => {
+    try{
+        const {name, email, password} = req.body
+
+        // cheching if the user is already exists
+
+        const userExists = await User.findOne({ email })
+        if(userExists) {
+            return res.status(400).json({ message: "User already exists"})
+        }
+
+        // Hashing the Password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Creating a new user
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            token: generateToken(user._id, user.role)
+        })
+
+
+    } catch( error) {
+        res.status(500).json({ msg: error.message })
+    }
+}
+
+
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        const user = await findOne({ email })
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                token: generateToken(user._id, user.role)
+            });
+        } else {
+            res.status(401).json({ message: "Invalid email or password"})
+        }
+    } catch(error) {
+        res.status(500).json({message : error.message})
+    }
+
+}
