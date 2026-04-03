e6a6d85954e95f0e93571bdb8403f3ec3fab51ac
Sanjay <sanjutech9959@gmail.com>
2026-04-03 19:20:31 +0530

    add user model with roles and status

---
+++ models/User.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'analyst', 'viewer'],
+        default: 'viewer'
+    },
+    inActive: {
+        type: Boolean,
+        default: true
+    }
+},
+    {timestamps: true}
+)
