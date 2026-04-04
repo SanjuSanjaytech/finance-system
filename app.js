882f144bc59085b9f301370545a1380f98b0aaba
Sanjay <sanjutech9959@gmail.com>
2026-04-05 01:04:05 +0530

    implemented role-based access control middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import authRoutes from "./routes/authRoutes.js"
 
+
 dotenv.config()
 
 const app = express()
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api/auth", authRoutes)
+
 
 
 // route
